bacc07f023de8b4554bc6013c0675759c505324a
Abewickrama Disanayaka Shanaka Sanjeewa <114850380+ShanakaSanjeewa@users.noreply.github.com>
2023-02-01 11:54:28 +0200

    modified
    added a image

--- layout/ex1.php
+++ layout/ex1.php
@@ -10,4 +10,47 @@ echo "Hello world! My name is \"Shanaka\"";
 ?>
     <h1>current date  </h1>
 <?php echo date("m.d.Y")?>
+
+
+<h3>3.4 variable in php </h3>
+<?php
+$title = "php is interesting";
+echo "<h1>" . $title . "</h1>";
+?>
+
+<h3>3.4 variable in php </h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+
+echo "
+<table border=1>
+<tr>
+<th> S.N </th><th> Name </th><th> Grade </th>
+</tr>
+
+<tr>
+<th> 1 </th><th> Pekka </th><th> $g1</th>
+</tr>
+
+<tr>
+<th> 2 </th><th> Johanna </th><th> $g2 </th>
+</tr>
+
+<tr>
+<th> 2 </th><th> John </th><th> $g3 </th>
+</tr>
+
+</table>
+";
+
+?>
+<h3> 4 screenshot of the development envireroment </h3>
+<img src="images/docker.png" alt="docker">
+
+
+
+
 <?php include"footer.php" ?>
+
