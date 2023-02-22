a84797f5906ac50c484c4e62138a450e5c10bfe0
Abewickrama Disanayaka Shanaka Sanjeewa <114850380+ShanakaSanjeewa@users.noreply.github.com>
2023-02-22 15:44:49 +0200

    java script added

---
+++ layout/site.js
@@ -0,0 +1,51 @@
+// single function to triger alert box
+function hello(){
+    alert("Welcome to my website");
+}
+
+//display or like echo
+//document.write
+//window.alert
+
+//simple function to add 2 numbers
+function add(){
+    document.write(5+6);
+}
+
+//document.getElementById(id)
+
+//function to change the background color of the page
+//function changeColor(where,newColor){
+ //   if(where =="background"){
+   //     document.body.style.backgroundColor=newColor;
+ //   }
+//}
+
+function changeColor(){
+    let color = document.getElementById('background').value;
+    document.body.style.backgroundColor = color;
+   
+}
+function changeFcolor(){
+    let fcolor=document.getElementById('fcolor').value;
+    document.body.style.color=fcolor;
+}
+
+//crud
+function crud1(){
+
+    let fname=(document.form1.fname.value).trim();
+    if (fname.length<5){
+        alert ("fisrt name must have ateast 5 character");
+        return false;
+    }
+}
+
+function fname(){
+
+    let fname=(document.form1.fname.value).trim();
+    if (fname.length<5){
+        alert ("fisrt name must have ateast 5 character");
+        return false;
+    }
+}
